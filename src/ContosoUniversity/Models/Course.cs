8308c4edfd37fd4a8a93319c1fee49f6e6b326f1
marco.rosario <marco.rosario@GFIPT0315>
2016-11-18 16:31:54 +0000

    Add project files.

---
+++ src/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(50, ErrorMessage = "The Title cannot be longer than 50 characters.")]
+        public string Title { get; set; }
+
+        [Range(0, 5)]
+        public int Credits { get; set; }
+
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> Assignments { get; set; }
+    }
+}
